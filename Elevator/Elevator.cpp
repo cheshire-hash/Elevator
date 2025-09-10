30e9cc48775d19d08ce99fbf5ca952fc83976ffb
Mandi_pv66 <Mandi_pv66@29-3>
2025-09-10 14:46:46 +0300

    /

--- Elevator/Elevator.cpp
+++ Elevator/Elevator.cpp
@@ -3,18 +3,26 @@ using namespace std;
 class Floor {
 	const int SIZE;
 public: 
-	void StatusFl();
-	int SetRange();
+	void StatusFl() {
+
+	}
+	int SetRange(int s) : SIZE(s)
+	}
 };
 class Elevator {
 	char* model;
 	Floor fl;
 public: ;
-	void ActiveEl();
-	void DeactivateEl();
-	void CallEl();
-	void ShowFloor();
-	void StatusEl();
+	  void ActiveEl() {
+	  }
+	  void DeactivateEl() {
+	  }
+	  void CallEl() {
+	  }
+	  void ShowFloor() {
+	  }
+	  void StatusEl() {
+	  }
 
 };
 int main() {
