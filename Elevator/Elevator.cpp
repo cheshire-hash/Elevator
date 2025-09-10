ef1487a7166530ab4e8d57d4e67b43244a302411
Mandi_pv66 <Mandi_pv66@29-3>
2025-09-10 14:39:00 +0300

    Add project files.

---
+++ Elevator/Elevator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+class Floor {
+	const int SIZE;
+public: 
+	void StatusFl();
+	int SetRange();
+};
+class Elevator {
+	char* model;
+	Floor fl;
+public: ;
+	void ActiveEl();
+	void DeactivateEl();
+	void CallEl();
+	void ShowFloor();
+	void StatusEl();
+
+};
+int main() {
+
+}
